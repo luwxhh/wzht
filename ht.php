5eb3cba65594c6a7cba434dd77a84a3cb44d0ef4
luwxhh <luwxhh@163.com>
2017-10-10 21:06:16 +0800

    Update ht.php

--- ht.php
+++ ht.php
@@ -27,6 +27,7 @@ $dbname = "test";
 
 // Create connection
 $conn = new mysqli( $servername, $username, $password, $dbname );
+$conn->query("SET NAMES UTF8");
 // Check connection
 if ( $conn->connect_error ) {
 	die( "Connection failed: " . $conn->connect_error );
